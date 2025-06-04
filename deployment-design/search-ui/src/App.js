f4c81ce17cd470ea6921435692162dcd6b4687dd
wendy <wendy.aw.wen.ting@gmail.com>
2025-06-04 16:13:42 +0800

    ui files

---
+++ deployment-design/search-ui/src/App.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
+
+import {
+  ErrorBoundary,
+  Facet,
+  SearchProvider,
+  SearchBox,
+  Results,
+  PagingInfo,
+  ResultsPerPage,
+  Paging,
+  Sorting,
+  WithSearch
+} from "@elastic/react-search-ui";
+import { Layout } from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+
+import {
+  buildAutocompleteQueryConfig,
+  buildFacetConfigFromConfig,
+  buildSearchOptionsFromConfig,
+  buildSortOptionsFromConfig,
+  getConfig,
+  getFacetFields
+} from "./config/config-helper";
+
+const connector = new ElasticsearchAPIConnector({
+  host: "http://localhost:9200",
+  index: "cv-transcriptions"
+});
+const config = {
+  searchQuery: {
+    facets: buildFacetConfigFromConfig(),
+    ...buildSearchOptionsFromConfig()
+  },
+  autocompleteQuery: buildAutocompleteQueryConfig(),
+  apiConnector: connector,
+  alwaysSearchOnInitialLoad: true
+};
+
+export default function App() {
+  return (
+    <SearchProvider config={config}>
+      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+        {({ wasSearched }) => {
+          return (
+            <div className="App">
+              <ErrorBoundary>
+                <Layout
+                  header={<SearchBox autocompleteSuggestions={true} />}
+                  sideContent={
+                    <div>
+                      {wasSearched && (
+                        <Sorting
+                          label={"Sort by"}
+                          sortOptions={buildSortOptionsFromConfig()}
+                        />
+                      )}
+                      {getFacetFields().map(field => (
+                        <Facet key={field} field={field} label={field} />
+                      ))}
+                    </div>
+                  }
+                  bodyContent={
+                    <Results
+                      titleField={getConfig().titleField}
+                      urlField={getConfig().urlField}
+                      thumbnailField={getConfig().thumbnailField}
+                      shouldTrackClickThrough={true}
+                    />
+                  }
+                  bodyHeader={
+                    <React.Fragment>
+                      {wasSearched && <PagingInfo />}
+                      {wasSearched && <ResultsPerPage />}
+                    </React.Fragment>
+                  }
+                  bodyFooter={<Paging />}
+                />
+              </ErrorBoundary>
+            </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
+  );
+}
